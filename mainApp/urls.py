13a17191cfcc74cc7e37857e22eb1d271c50d823
denissemo <denissemo1999@gmail.com>
2019-03-16 21:25:19 +0200

    update

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LogoutView
 from . import views
-from .views import SimpleWeatherView
+from .views import SimpleWeatherView, SignUpView, LogInView
 
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('current-city/', SimpleWeatherView.as_view(), name='current-city')
+    path('current-city/', SimpleWeatherView.as_view(), name='current-city'),
+    path('sign_up/', SignUpView.as_view(), name="sign_up"),
+    path('log_in/', LogInView.as_view(), name="log_in"),
+    # path('user_account/<user>/', UserAccountView.as_view(),
+    #      name="account_view"),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy("index")),
+         name="logout")
 ]
