0c94aad8f302635ab5a5c54db709c7bef7d6d3e7
denissemo <denissemo1999@gmail.com>
2019-03-16 21:10:58 +0200

    add models of User and City and migrate

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=200)
+    temp = models.CharField(max_length=10)
+    description = models.CharField(max_length=200)
+    wind = models.CharField(max_length=10)
+    clouds = models.CharField(max_length=10)
+
+    def __str__(self):
+        return "{}, {}°C".format(self.name, self.temp)
+
+
+class UserAccount(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    cities = models.ManyToManyField(City)
+
+    def __str__(self):
+        return self.user.username
+
+    # def get_absolute_url(self):
+    #     return reverse("account_view", kwargs={"user": self.user.name})
+
+
