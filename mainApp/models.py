1fa1d3e8b0ea124e853aee7a5197949c10c2a21c
denissemo <denissemo1999@gmail.com>
2019-04-17 21:43:46 +0300

    update models, add new models

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,30 +1,19 @@
 from django.db import models
-from django.conf import settings
-from django.urls import reverse
 
 
 class City(models.Model):
-    name = models.CharField(max_length=200)
-    temp = models.CharField(max_length=10)
-    description = models.CharField(max_length=200)
-    wind = models.CharField(max_length=10)
-    clouds = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
 
     def __str__(self):
-        return "{}, {}°C".format(self.name, self.temp)
+        return self.name
 
 
-class UserAccount(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email = models.EmailField()
-    cities = models.ManyToManyField(City)
-
-    def __str__(self):
-        return self.user.username
-
-    # def get_absolute_url(self):
-    #     return reverse("account_view", kwargs={"user": self.user.name})
-
-
+class Day(models.Model):
+    date = models.CharField(max_length=30)
+    min_temp = models.CharField(max_length=5)
+    max_temp = models.CharField(max_length=5)
+    wind = models.CharField(max_length=5)
+    description = models.CharField(max_length=100)
+    clouds = models.CharField(max_length=5)
+    icon = models.CharField(max_length=5)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
