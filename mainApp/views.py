6b0882eeb66d51a95e3447da33c05f9f2c371c28
denissemo <denissemo1999@gmail.com>
2019-04-17 21:46:08 +0300

    update main page and cities page views

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-from django.contrib.auth import get_user_model, authenticate, login
-from django.http import HttpResponseRedirect
 from django.views import View
-
-from .weather_models import SimpleWeather
-from .forms import SignUpForm, LogInForm
-from .models import UserAccount
+from .weather_models import Weather
+from .models import City, Day
+from django.http import HttpResponse
+import calendar
+import datetime
 
 
 def index(request):
     return render(request, 'mainPage/index.html')
 
 
-class SimpleWeatherView(View):
-    template = 'current-city/current-city.html'
-
-    def get(self, *args, **kwargs):
-        simpleWeather = SimpleWeather(self.request.GET['city'])
-        # this use for jinja
-        context = {
-            'cities': simpleWeather
-        }
-        return render(self.request, self.template, context)
+def cities(request):
+    template = 'city/cities_template.html'
+    city = City.objects.in_bulk()  # return data in dict format example: {1: <City: Vinnytsia>, 2: <City: Kyiv>}
+    day = Day.objects.in_bulk()
+    days = list(set([day[id_].date for id_ in day]))
+    days.sort()
+    context = {
+        'cities': [city[id_].name for id_ in city],
+        'days': days
+    }
+    return render(request, template, context)
